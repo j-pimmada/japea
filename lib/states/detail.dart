f4ea2868496e43986badc2ceb348fb977c0bd244
Pimmada <pimmada.ran@pea.co.th>
2022-06-24 09:05:54 +0700

    start day 5

--- lib/states/detail.dart
+++ lib/states/detail.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'package:japea/models/job_model.dart';
 import 'package:japea/utility/my_constant.dart';
+import 'package:japea/utility/my_dialog.dart';
+import 'package:japea/widgets/show_icon_button.dart';
+import 'package:japea/widgets/show_image.dart';
+import 'package:japea/widgets/show_text.dart';
 
-class Detail extends StatelessWidget {
+class Detail extends StatefulWidget {
   final JobModel jobModel;
   const Detail({
     Key? key,
@@ -12,12 +19,111 @@ class Detail extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<Detail> createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  JobModel? jobModel;
+  File? file;
+
+  @override
+  void initState() {
+    super.initState();
+    jobModel = widget.jobModel;
+  }
+
+  Future<void> processTakePhoto({required ImageSource imageSource}) async {
+    var result = await ImagePicker().pickImage(
+      source: imageSource,
+      maxWidth: 800,
+      maxHeight: 800,
+    );
+
+    if (result != null) {
+      file = File(result.path);
+      setState(() {});
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
+    return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        title: ShowText(
+          text: jobModel!.job,
+          textStyle: MyConstant().h2Style(),
+        ),
         backgroundColor: Colors.white,
         foregroundColor: MyConstant.dark,
       ),
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints boxConstraints) {
+        return ListView(
+          children: [
+            newImage(boxConstraints),
+            newDetail(boxConstraints),
+          ],
+        );
+      }),
+    );
+  }
+
+  Row newDetail(BoxConstraints boxConstraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 16),
+          width: boxConstraints.maxWidth * 0.6,
+          child: ShowText(text: jobModel!.detail),
+        ),
+      ],
+    );
+  }
+
+  Row newImage(BoxConstraints boxConstraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 36, bottom: 16),
+          width: boxConstraints.maxWidth * 0.6,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: file == null
+                    ? const ShowImage(
+                        path: 'images/image.png',
+                      )
+                    : Image.file(file!),
+              ),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: ShowIconButton(
+                    iconData: Icons.add_a_photo,
+                    pressFunc: () {
+                      MyDialog(context: context).normalDialog(
+                          label: 'Camera',
+                          label2: 'Gallery',
+                          pressFunc: () {
+                            processTakePhoto(imageSource: ImageSource.camera);
+                            Navigator.pop(context);
+                          },
+                          pressFunc2: () {
+                            processTakePhoto(imageSource: ImageSource.gallery);
+                            Navigator.pop(context);
+                          },
+                          title: 'Source Image',
+                          subTitle: 'Please Tab Camera or Gallery');
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
